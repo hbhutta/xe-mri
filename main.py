19f1f6fda3e3293f0b4d98ffbde6c7629de8d129
Haad Bhutta <112280419+hbhutta@users.noreply.github.com>
2024-11-27 15:35:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from classes.Image import Image
+from absl import app, flags
+import numpy as np
+from classes.Image import NII
 from utils.utils import *
-from utils.enums import ReorientKey, Direction
+from utils.enums import ReorientKey, Direction, SFORM_CODE
 
 import nibabel as nib
 # from scripts.reorient import reorient
@@ -11,10 +13,14 @@ import nibabel as nib
 
 import os
 
-import numpy as np
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+logging.basicConfig(filename='log.log', encoding='utf-8',
+                    format='%(asctime)s %(message)s', level=logging.DEBUG)
+
 np.set_printoptions(precision=20)
 
-from absl import app, flags
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string(name="dir", default=None, help="""
@@ -33,9 +39,10 @@ flags.DEFINE_string(name="dir", default=None, help="""
                     just for a single patient.
                     """, required=True)
 
-'''
+
 def process(patient_dir: str) -> None:
     # Minimum set of files that must exist for any patient, with exactly these names
+
     original_files = get_files(dir=patient_dir, files=[
         "CT.nii",
         "CT_mask.nii",
@@ -45,12 +52,38 @@ def process(patient_dir: str) -> None:
         "membrane2gas.nii"
     ])
 
+    ct_img = NII(filename=original_files[0])
+    ct_mask = NII(filename=original_files[1])
+
+    if not (ct_img.get_axcodes() == tuple("RAS") and ct_mask.get_axcodes() == tuple("RAS")):
+        logging.info(f"Detected non-RAS orientations for files:{ct_img.get_filename()} and {ct_mask.get_filename()}")
+        for img in [ct_img, ct_mask]:
+            # Ensure that the reference and mask both have pixdim[0] != 0
+            if not img.get_qfac():
+                logging.info(
+                    f"{img.get_filename()} has pixdim[0] = 0. Setting to 1.")
+                img.set_qfac(1)
+
+            # Ensure that the srow matrix will be used by setting the sform_code to 1
+            if (img.get_sform_code() == 0):
+                logging.info(
+                    f"{img.get_filename()} has sform_code = 0. Setting to 1 (scanner).")
+                img.set_sform_code(1)
+
+        x, y, z = ct_img.get_origin()
+        ct_mask.translate(-x, -y, -z)
+    else:
+        """In the case where both the reference and the mask are in RAS, 
+        is sufficient to translate the mask to match the origin of the reference"""
+        x, y, z = ct_img.get_origin()
+        ct_mask.translate(-x, -y, -z)
+
     # for fn in original_files[1:]:  # Resize all files except for CT_mask.nii
     #     print(patient_dir)
     #     print(os.path.basename(fn))
     #     resize(img_path=fn)
 
-    post_resize = get_files(dir=patient_dir, files=[
+    post_resize_files = get_files(dir=patient_dir, files=[
         "CT.nii",
         "CT_mask.nii",
         "mask_reg_edited_scaled.nii",
@@ -59,92 +92,71 @@ def process(patient_dir: str) -> None:
         "membrane2gas_scaled.nii"
     ])
 
-    for fn in post_resize:
-        if "CT_mask" in fn:
-            reorient(file=fn, key=ReorientKey.CT.value)
-        else:
-            reorient(file=fn, key=ReorientKey.VENT.value)
+    # e.g. PIm0051
 
-    files = [  # After resizing and reorienting
-        "CT_nii",
-        "CT_mask_neg_affine.nii",
-        "mask_reg_edited_scaled_mutated_affine.nii",
-        "gas_highreso_scaled_mutated_affine.nii",
-        "rbc2gas_scaled_mutated_affine.nii",
-        "membrane2gas_scaled_mutated_affine.nii"
-    ]
+#    ct_mask.translate(x,y,z)
 
-    ct_file, mr_file, gas, rbc, mem = [
-        os.path.join(patient_dir, fn) for fn in files]
+#    ct_mask.save(filename="CT_mask_translated_216.nii")
 
-    # Assert files exist *after* reorienting and resizing
-    for fn_path in [os.path.join(patient_dir, fn) for fn in files]:
-        if not os.path.exists(fn_path):
-            raise Exception(f"""
-                  File {fn_path} does not exist for patient {patient_dir[5:]}
-                  but it is required for processing them in this pipeline.
-                  """)
+    # if (ct_img.axcodes() == tuple("RAI")):
+    #    # ct_img.set_qform()
+    #    pass
+    # elif (ct_img.axcodes() == tuple("RAS")):
+    #     pass
 
-    print(f"ct {ct_file} | mr {mr_file} | gas {gas} | rbc {rbc} | mem {mem}")
+#    ct_mask.translate(x=0,y=0, z=128)
+    # align(img=ct_mask, ref=ct_img)
 
-    register(ct_filename=ct_file, mri_filename=mr_file, dir=patient_dir)
-    unpack(ct_file=ct_file, dir=patient_dir)
-    unzip(os.path.join(patient_dir, "warped_mri.nii.gz"))
-
-    for ven in [gas, rbc, mem]:
-        warp_vent(ct=ct_file, dir=patient_dir, vent=ven)
-
-'''
-     
-def align(img: Image, ref: Image) -> None:
-    ref_qform_code = ref.get_qform_code()
-    ref_sform_code = ref.get_sform_code()
-    pass
+#    for fn in post_resize_files:
+#        if "CT_mask" in fn:
+#            reorient(file=fn, key=ReorientKey.CT.value)
+#        else:
+#            reorient(file=fn, key=ReorientKey.VENT.value)
+#
+#    files = [  # After resizing and reorienting
+#        "CT_nii",
+#        "CT_mask_neg_affine.nii",
+#        "mask_reg_edited_scaled_mutated_affine.nii",
+#        "gas_highreso_scaled_mutated_affine.nii",
+#        "rbc2gas_scaled_mutated_affine.nii",
+#        "membrane2gas_scaled_mutated_affine.nii"
+#    ]
+#
+#    ct_file, mr_file, gas, rbc, mem = [
+#        os.path.join(patient_dir, fn) for fn in files]
+#
+#    # Assert files exist *after* reorienting and resizing
+#    for fn_path in [os.path.join(patient_dir, fn) for fn in files]:
+#        if not os.path.exists(fn_path):
+#            raise Exception(f"""
+#                  File {fn_path} does not exist for patient {patient_dir[5:]}
+#                  but it is required for processing them in this pipeline.
+#                  """)
+#
+#    print(f"ct {ct_file} | mr {mr_file} | gas {gas} | rbc {rbc} | mem {mem}")
+#
+#    register(ct_filename=ct_file, mri_filename=mr_file, dir=patient_dir)
+#    unpack(ct_file=ct_file, dir=patient_dir)
+#    unzip(os.path.join(patient_dir, "warped_mri.nii.gz"))
+#
+#    for ven in [gas, rbc, mem]:
+#        warp_vent(ct=ct_file, dir=patient_dir, vent=ven)
+#
+#
 
 
 def main(argv):
-    ref = Image(filename="imgs/PIm0216/CT.nii")
-    img = Image(filename="imgs/PIm0216/CT_mask.nii")
-
-    # 1. Check if ref is using sform or qform
-    print(ref.get_qform_code()) # 1
-    print(ref.get_sform_code()) # 0
-    
-    print(img.get_qform_code()) # 0
-    print(img.get_sform_code()) # 0
-
-    print(img.is_matched_by_qform(ref))    
-    print(img.is_matched_by_sform(ref))    
-
-
-    print(img.get_qform())
-    print(img.get_sform())
-    print(ref.get_qform())
-    print(ref.get_sform())
-    
-    print(img.get_origin())
-    print(ref.get_origin())
-
-#    img.set_qform(qform = ref.get_qform())
-    
-    
-    
-    
-
-    
-
-    #img.save(filename=img.get_filename()[:-4] + "_aligned_new.nii")
-    # dir_ = FLAGS.dir
-    # print(dir_)
-    # if has_sub_dirs(dir_):
-    #     print("Recieved a batch directory of multiple patients.")
-    #     for subdir in get_subdirs(dir_):
-    #         print(f"Processing patient {subdir[5:]}")
-    #         process(patient_dir=subdir)
-    # else:
-    #     print("Recieved a directory of single patient.")
-    #     print(f"Processing patient {dir_[5:]}")
-    #     process(patient_dir=dir_)
+    dir_ = FLAGS.dir
+    print(dir_)
+    if contains_subdir(dir_):
+        print("Recieved a batch directory of multiple patients.")
+        for subdir in get_subdirs(dir_):
+            print(f"Processing patient {subdir[5:]}")
+            process(patient_dir=subdir)
+    else:
+        print("Recieved a directory of single patient.")
+        print(f"Processing patient {dir_[5:]}")
+        process(patient_dir=dir_)
 
 
 if __name__ == "__main__":
