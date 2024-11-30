77cc18b2c720e9036dcaf556c2a1d47ffccb2fcb
Haad Bhutta <haadbhutta@gmail.com>
2024-11-30 11:28:12 -0800

    Update logger

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
 import logging
 logger = logging.getLogger(__name__)
-#FORMAT = '%(message)s'
 FORMAT ='[%(levelname)s %(asctime)s %(filename)s->%(funcName)s():%(lineno)s]: %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 logger.setLevel(logging.DEBUG)
