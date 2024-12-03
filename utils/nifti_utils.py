d2dc8b2e374db589e359b970588a89935526819a
hbhutta <haadbhutta@gmail.com>
2024-12-03 23:38:02 +0000

    Made wrappers

--- utils/nifti_utils.py
+++ utils/nifti_utils.py
@@ -1,18 +1,25 @@
 import subprocess
 
+
 def mod_field(nii: str, field: str, value):
-    out = subprocess.run(["nifti_tool",
-                          "-mod_hdr", "-mod_field", field, str(value),
-                          "-infiles",  nii,
-                          "-overwrite"])
+    subprocess.run(["nifti_tool",
+                    "-mod_hdr", "-mod_field", field, str(value),
+                    "-infiles",  nii,
+                    "-overwrite"])
+
 
 def disp_field(nii: str, field: str):
-    out = subprocess.run(["nifti_tool",
-                          "-disp_hdr1", "-field", field,
-                          "-infiles",  nii])
+    subprocess.run(["nifti_tool",
+                    "-disp_hdr1", "-field", field,
+                    "-infiles",  nii])
+    
+def diff_hdr(nii_1: str, nii_2: str):
+    subprocess.run(["nifti_tool",
+                    "-diff_hdr", 
+                    "-infiles",  nii_1, nii_2])
+ 
 
 # Example
 # disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
 # mod_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code", 1)
 # disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
-
