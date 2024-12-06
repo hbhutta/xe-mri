6fde48eb767c6935315b3b8f47aa76ed22a48421
hbhutta <haadbhutta@gmail.com>
2024-12-06 18:57:17 +0000

    Update os utils

--- utils/os_utils.py
+++ utils/os_utils.py
@@ -1,11 +1,9 @@
 import os
 
-
 def get_files(dir: str, files: list[str] | str) -> list[str] | str:
     if type(files) == str:
         return os.path.join(dir, files)
     return [os.path.join(dir, fn) for fn in files]
-
 
 def contains_subdir(dir_: str | os.PathLike) -> bool:
     for item in os.listdir(dir_):
@@ -14,6 +12,8 @@ def contains_subdir(dir_: str | os.PathLike) -> bool:
             return True
     return False
 
-
 def get_subdirs(dir_: str | os.PathLike) -> list[str]:
     return [os.path.join(dir_, subdir) for subdir in os.listdir(dir_)]
+
+def list2str(ls: list):
+    return " ".join(str(e) for e in ls)
