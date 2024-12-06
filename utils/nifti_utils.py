aff301177f0aa114c19aa8ac83d5e69430920019
hbhutta <haadbhutta@gmail.com>
2024-12-06 18:57:41 +0000

    Update nifti utils

--- utils/nifti_utils.py
+++ utils/nifti_utils.py
@@ -1,9 +1,10 @@
 import subprocess
 
-
 def mod_field(nii: str, field: str, value):
+    if type(value) != str:
+        value = str(value)
     subprocess.run(["nifti_tool",
-                    "-mod_hdr", "-mod_field", field, str(value),
+                    "-mod_hdr", "-mod_field", field, value,
                     "-infiles",  nii,
                     "-overwrite"])
 
@@ -18,8 +19,10 @@ def diff_hdr(nii_1: str, nii_2: str):
                     "-diff_hdr", 
                     "-infiles",  nii_1, nii_2])
  
+"""
+Example:
 
-# Example
-# disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
-# mod_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code", 1)
-# disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
+disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
+mod_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code", 1)
+disp_field("imgs_copy_new/PIm0216/CT_copy.nii", "sform_code")
+"""
